e484312ebef3216948a36e92eee066a61ca1071d
tcfwbper <b05208031@ntu.edu.tw>
2026-01-11 22:07:32 +0800

    refactor(dev, pyproject, flake8): update development tools

--- dev/test.sh
+++ dev/test.sh
@@ -7,39 +7,35 @@ echo "=== test.sh ==="
 echo "- Start Python checks"
 
 echo "- isort: start"
-python -m isort --check-only src/my_python_project
+python -m isort --check-only src/thc_backend_server
 echo "- isort: done"
 
 echo "- black: start"
-python -m black --check src/my_python_project
+python -m black --check src/thc_backend_server
 echo "- black: done"
 
 echo "- docformatter: start"
-python -m docformatter -c -r src/my_python_project
+python -m docformatter -c -r src/thc_backend_server
 echo "- docformatter:  done"
 
 echo "- ruff: start"
-python -m ruff check src/my_python_project
+python -m ruff check src/thc_backend_server
 echo "- ruff: done"
 
 echo "- mypy: start"
-python -m mypy src/my_python_project
+python -m mypy src/thc_backend_server
 echo "- mypy: done"
 
 echo "- pylint: start"
-python -m pylint src/my_python_project
+python -m pylint src/thc_backend_server
 echo "- pylint: done"
 
 echo "- flake8: start"
-python -m flake8 src/my_python_project
+python -m flake8 src/thc_backend_server
 echo "- flake8: done"
 
 echo "- pytest: start"
-if [[ "$1" == "--skip-slow" ]]; then
-  python -m pytest --cov=src/my_python_project --disable-warnings -m "not slow"
-else
-  python -m pytest --cov=src/my_python_project --disable-warnings
-fi
+python -m pytest --cov=src/thc_backend_server --disable-warnings
 echo "- pytest: done"
 
 echo "- All Python checks passed"
---
+++ dev/uv-install.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/../
+
+pip install uv==0.6.12
